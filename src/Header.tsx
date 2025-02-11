49e008476f7a6e2bd98462ba0b0a9f8b85c40723
Ahtoh1337 <uvo1anton@gmail.com>
2025-02-11 20:18:45 +0200

    Style header

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,7 +1,23 @@
 import { observer } from "mobx-react-lite";
 
 export const Header = observer(function () {
-    return <div >
-        <h1 className="text-2xl">{import.meta.env.VITE_APP_TITLE}</h1>
+    return <div className="w-full p-3 pr-
+    bg-indigo-200 drop-shadow-lg
+    flex justify-between">
+        <div className="text-2xl pl-2 pr-3
+        border-l-2 border-b-2
+        border-l-indigo-900 border-b-indigo-900 rounded-l-full
+        text-indigo-900
+        bg-gradient-to-r from-indigo-500/40
+        drop-shadow-sm
+        first-letter:font-bold">
+            {import.meta.env.VITE_APP_TITLE}
+        </div>
+        <button className="bg-linear-to-r from-indigo-400 to-indigo-700
+        px-2 rounded-l-full rounded-r-full
+        drop-shadow-sm border-2 border-indigo-50
+        hover:from-indigo-500 hover:to-indigo-800 hover:border-indigo-300">
+            ☀️ 🌙
+        </button>
     </div>
 })
